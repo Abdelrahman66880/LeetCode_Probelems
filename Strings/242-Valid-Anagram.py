2368aefef5b3087eeb5af4823f96016b8f01e09d
Abdelrahman_Mohammed <111823552+Abdelrahman66880@users.noreply.github.com>
2024-10-31 17:01:38 +0300

    [242] [Time Beats: 65.17%] [Memory Beats: 8.59%] - LeetPush

--- Strings/242-Valid-Anagram.py
+++ Strings/242-Valid-Anagram.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         \\\
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False 
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        for j in countS:
+            if countS[j] != countT.get(j, 0):
+                return False
+        
+        return True
