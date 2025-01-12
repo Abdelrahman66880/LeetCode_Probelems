1ab6078ea5721903c35abbf34f7a5ae1019fa0b3
Abdelrahman_Mohammed <111823552+Abdelrahman66880@users.noreply.github.com>
2025-01-13 01:13:38 +0200

    [21] [Time Beats: 100.00%] [Memory Beats: 35.12%] - LeetPush

---
+++ DataStructure/21-Merge-Two-Sorted-Lists.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+//  no need of helper function
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    if(list1 == NULL)
+    {
+        return list2;
+    }
+    if(list2 == NULL)
+    {
+        return list1;
+    }
+
+
+    if (list1->val <= list2->val)
+    {
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    }else{
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
+
+
+}
