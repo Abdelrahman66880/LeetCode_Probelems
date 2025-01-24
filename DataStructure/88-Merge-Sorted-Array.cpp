5a80e7360ca26acf954596a9ef634d597f7b9b6f
Abdelrahman_Mohammed <111823552+Abdelrahman66880@users.noreply.github.com>
2025-01-24 14:05:09 +0200

    [88] [Time Beats: 100.00%] [Memory Beats: 37.75%] - LeetPush

---
+++ DataStructure/88-Merge-Sorted-Array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int last = m + n - 1;
+        m--;
+        n--;
+        while (m>= 0 && n >= 0)
+        {
+            if(nums1[m] > nums2[n])
+            {
+                nums1[last] = nums1[m];
+                m--;
+
+            }else
+            {
+                nums1[last] = nums2[n];
+                n--;
+            }
+            last--;
+        }
+        while(n >= 0)
+        {
+            nums1[last] = nums2[n];
+            n--;
+            last--;
+        }
+    }
+};
